50f08a829e83c44cbaa8493850eec06c6506c4ff
tjumma <tim@jumma.pro>
2023-05-16 14:48:13 +0300

    ClientNetworkTransform

---
+++ Assets/Shakalaka/ClientNetworkTransform.cs
@@ -0,0 +1,14 @@
+﻿using Unity.Netcode.Components;
+using UnityEngine;
+
+namespace Shakalaka
+{
+    [DisallowMultipleComponent]
+    public class ClientNetworkTransform : NetworkTransform
+    {
+        protected override bool OnIsServerAuthoritative()
+        {
+            return false;
+        }
+    }
+}
---
+++ Assets/Shakalaka/PlayerNetwork.cs
@@ -0,0 +1,24 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+namespace Shakalaka
+{
+    public class PlayerNetwork : NetworkBehaviour
+    {
+        [SerializeField] private float moveSpeed;
+        
+        private void Update()
+        {
+            if (!IsOwner) return;
+            
+            var moveDir = Vector3.zero;
+
+            if (Input.GetKey(KeyCode.W)) moveDir.z = +1f;
+            if (Input.GetKey(KeyCode.D)) moveDir.x = +1f;
+            if (Input.GetKey(KeyCode.S)) moveDir.z = -1f;
+            if (Input.GetKey(KeyCode.A)) moveDir.x = -1f;
+
+            transform.position += moveDir * (moveSpeed * Time.deltaTime);
+        }
+    }
+}

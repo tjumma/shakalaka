5debf65eece051b4902c5e68688c96a376ebc605
tjumma <tim@jumma.pro>
2023-05-17 17:09:28 +0300

    Server authoritative movement

--- Assets/Shakalaka/PlayerNetwork.cs
+++ Assets/Shakalaka/PlayerNetwork.cs
@@ -6,19 +6,54 @@ namespace Shakalaka
     public class PlayerNetwork : NetworkBehaviour
     {
         [SerializeField] private float moveSpeed;
+
+        private Vector3 _serverMoveInput;
         
+        
+
         private void Update()
         {
-            if (!IsOwner) return;
-            
-            var moveDir = Vector3.zero;
+            var moveInput = Vector3.zero;
 
-            if (Input.GetKey(KeyCode.W)) moveDir.z = +1f;
-            if (Input.GetKey(KeyCode.D)) moveDir.x = +1f;
-            if (Input.GetKey(KeyCode.S)) moveDir.z = -1f;
-            if (Input.GetKey(KeyCode.A)) moveDir.x = -1f;
+            if (Input.GetKey(KeyCode.W)) moveInput.z = +1f;
+            if (Input.GetKey(KeyCode.D)) moveInput.x = +1f;
+            if (Input.GetKey(KeyCode.S)) moveInput.z = -1f;
+            if (Input.GetKey(KeyCode.A)) moveInput.x = -1f;
 
-            transform.position += moveDir * (moveSpeed * Time.deltaTime);
+            if (IsServer)
+            {
+                if (IsLocalPlayer)
+                    _serverMoveInput = moveInput;
+                Move(_serverMoveInput);
+            }
+            else if(IsClient && IsLocalPlayer)
+                MoveServerRpc(moveInput);
+        }
+
+        private void Move(Vector3 moveInput)
+        {
+            transform.position += moveInput * (moveSpeed * Time.deltaTime);
+        }
+
+        [ServerRpc]
+        private void MoveServerRpc(Vector3 moveInput)
+        {
+            _serverMoveInput = moveInput;
+        }
+
+        public override void OnNetworkSpawn()
+        {
+            NetworkManager.Singleton.NetworkTickSystem.Tick += OnTick;
+        }
+        
+        public override void OnNetworkDespawn()
+        {
+            NetworkManager.Singleton.NetworkTickSystem.Tick -= OnTick;
+        }
+
+        private void OnTick()
+        {
+            // Debug.Log("Tick");
         }
     }
 }

76a8594b0bfa1d842d58b2a1d74f5bca55602771
tjumma <tim@jumma.pro>
2023-06-14 14:02:45 +0300

    Touch test

---
+++ Assets/Shakalaka/Scripts/CardSelector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace Shakalaka
+{
+    public class CardSelector : MonoBehaviour
+    {
+        [SerializeField] private InputManager inputManager;
+
+        private void OnEnable()
+        {
+            inputManager.OnFingerDown += TrySelectCard;
+        }
+        private void OnDisable()
+        {
+            inputManager.OnFingerDown -= TrySelectCard;
+        }
+        
+        private void TrySelectCard(Vector2 touchPosition, float time)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(touchPosition);
+
+            if (Physics.Raycast(ray, out var hit))
+            {
+                Destroy(hit.collider.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Shakalaka/Scripts/InputManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
+
+namespace Shakalaka
+{
+    public class InputManager : MonoBehaviour
+    {
+        public Action<Vector2, float> OnFingerDown;
+        
+        public Action<Vector2, float> OnStartTouch;
+        public Action<Vector2, float> OnEndTouch;
+        
+        private TouchControls _touchControls;
+
+        private void Awake()
+        {
+            _touchControls = new TouchControls();
+        }
+
+        private void OnEnable()
+        {
+            _touchControls.Enable();
+            EnhancedTouchSupport.Enable();
+            
+            _touchControls.Touch.TouchPress.started += StartTouch;
+            _touchControls.Touch.TouchPress.canceled += EndTouch;
+            UnityEngine.InputSystem.EnhancedTouch.Touch.onFingerDown += FingerDown;
+        }
+
+        private void OnDisable()
+        {
+            _touchControls.Disable();
+            EnhancedTouchSupport.Disable();
+            
+            _touchControls.Touch.TouchPress.started -= StartTouch;
+            _touchControls.Touch.TouchPress.canceled -= EndTouch;
+            UnityEngine.InputSystem.EnhancedTouch.Touch.onFingerDown -= FingerDown;
+        }
+
+        private void FingerDown(Finger finger)
+        {
+            Debug.Log($"FingerDown at {finger.screenPosition}");
+            OnFingerDown?.Invoke(finger.screenPosition, Time.time);
+        }
+
+        private void StartTouch(InputAction.CallbackContext ctx)
+        {
+            var touchPosition = _touchControls.Touch.TouchPosition.ReadValue<Vector2>();
+            Debug.Log($"Touch started at {touchPosition}");
+            OnStartTouch?.Invoke(touchPosition, (float)ctx.startTime);
+        }
+
+        private void EndTouch(InputAction.CallbackContext ctx)
+        {
+            var touchPosition = _touchControls.Touch.TouchPosition.ReadValue<Vector2>();
+            Debug.Log($"Touch ended at {touchPosition}");
+            OnEndTouch?.Invoke(touchPosition, (float)ctx.time);
+        }
+    }
+}
